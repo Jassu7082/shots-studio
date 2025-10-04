c6720fcdc3fbacb429439e3df987e50c0037a3b6
AnsahMohammad <mohdansah10@gmail.com>
2025-10-04 19:12:39 +0530

    refactor: remove unnecessary import of flutter/foundation.dart from privacy_screen and expandable_fab

--- shots_studio/lib/screens/privacy_screen.dart
+++ shots_studio/lib/screens/privacy_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shots_studio/services/snackbar_service.dart';
